52bbc7321c054fd337d6bf17a9ebdaf1f5fc0353
A Student <student@nowhere.edu>
2026-04-14 02:44:44 +0000

    Apr 13, 2026, 7:44 PM

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ using namespace std;
 
 //OUTPUT//
 //The horizontal speed of the bike when it leaves the ramp//
-//The time the bike stays in the air
-//The maximum height reached during the jump
-//The horizontal distance traveled before landing
-//The speed of the bike at landing
+//The time the bike stays in the air//
+//The maximum height reached during the jump//
+//The horizontal distance traveled before landing//
+//The speed of the bike at landing//
 
 const int acceleration_due_to_gravity = 32.17405;
 
