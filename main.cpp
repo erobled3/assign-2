59ca75d014b8a4908d8372cd5f8155b51bf9e0c2
A Student <student@nowhere.edu>
2026-04-14 03:06:32 +0000

    Apr 13, 2026, 8:06 PM

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
   int ramp_height = 12;
   cout << "Ramp height: " << ramp_height << endl;
   //TWO The launch velocity of the bike (in feet per second)//
-  int bike_velocity = 12;
+  int bike_velocity = 55;
   cout << "Bike Velocity: " << bike_velocity << endl;
 
   //THREE The launch angle of the ramp (in degrees)//
