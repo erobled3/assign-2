ad437cfeae404deadabf75196aac689bbdf52931
A Student <student@nowhere.edu>
2026-04-14 05:07:25 +0000

    Apr 13, 2026, 10:07 PM

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main() {
   //The time the bike stays in the air//
 
   double squared_vert_velocity= std::pow(vertical_velocity, 2);
-  double two_times_gravity_height = 
+  double two_times_gravity_height = 2 * GRAVITY 
 
 
   //cout << "Air Time: " << time_in_air << endl;
