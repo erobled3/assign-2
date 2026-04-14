a6d2853ff72b540e644614f39dae27749b76c90e
A Student <student@nowhere.edu>
2026-04-14 02:44:13 +0000

    Apr 13, 2026, 7:44 PM

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 using namespace std;
 
-//INPUT
+//INPUT//
 //ONE The height of the ramp above the landing area (in feet)//
 
 //TWO The launch velocity of the bike (in feet per second)//
 
 //THREE The launch angle of the ramp (in degrees)//
 
+//OUTPUT//
 The horizontal speed of the bike when it leaves the ramp
 The time the bike stays in the air
 The maximum height reached during the jump
