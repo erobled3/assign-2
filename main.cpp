c68cf437f657266adf772bd1f1d65b57aeaeee37
A Student <student@nowhere.edu>
2026-04-14 04:01:56 +0000

    Apr 13, 2026, 9:01 PM

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main() {
   double (bike_velocity * sin(angle_of_bike * PI/180));
 
   //The time the bike stays in the air//
-  cout << "Air Time" << time_in_air
+  cout << "Air Time" << time_in_air << endl;
 
   //The maximum height reached during the jump//
   //The horizontal distance traveled before landing//
