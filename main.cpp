411e43178d1651a92dc941d8ca4fdf668dc2bb26
A Student <student@nowhere.edu>
2026-04-14 02:51:04 +0000

    Apr 13, 2026, 7:51 PM

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 int main() 
 {
-    //INPUT//
-//ONE The height of the ramp above the landing area (in feet)//
+      //INPUT//
+    //ONE The height of the ramp above the landing area (in feet)//
 
-//TWO The launch velocity of the bike (in feet per second)//
+    //TWO The launch velocity of the bike (in feet per second)//
 
-//THREE The launch angle of the ramp (in degrees)//
+    //THREE The launch angle of the ramp (in degrees)//
 
-//OUTPUT//
-//The horizontal speed of the bike when it leaves the ramp//
-//The time the bike stays in the air//
-//The maximum height reached during the jump//
-//The horizontal distance traveled before landing//
-//The speed of the bike at landing//
+    //OUTPUT//
+    //The horizontal speed of the bike when it leaves the ramp//
+    //The time the bike stays in the air//
+    //The maximum height reached during the jump//
+    //The horizontal distance traveled before landing//
+    //The speed of the bike at landing//
 
-const double acceleration_due_to_gravity = 32.17405;
+    const double acceleration_due_to_gravity = 32.17405;
 
-std::numbers::pi
+    std::numbers::pi
 
 }
 
