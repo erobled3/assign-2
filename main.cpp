6e20ba93ffff715c74495206f06ba511e6c662f5
A Student <student@nowhere.edu>
2026-04-14 02:38:59 +0000

    Apr 13, 2026, 7:38 PM

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 #include <cmath>
 using namespace std;
 
+The height of the ramp above the landing area (in feet)
 
 // parameters like: "doubleIT(int x)"" DO NOT use cin or cout!! they use "return!"//
