ea65242d657aa88ab0f479594735df916e5f40d3
A Student <student@nowhere.edu>
2026-04-14 02:37:37 +0000

    Apr 13, 2026, 7:37 PM

--- main.cpp
+++ main.cpp
@@ -2,26 +2,5 @@
 
 using namespace std;
 
-void foo()
-{
-  cout << " Amaze amaze amaze! ";
-}
 
-void silly()
-{
-  cout << " squishy space blob ";
-}
-
-void burger()
-{
-  cout << " Grace Rocky save stars ";
-}
-
-int main()
-{
-  foo();
-  burger();
-  silly();
-  foo();
-}
 // parameters like: "doubleIT(int x)"" DO NOT use cin or cout!! they use "return!"//
