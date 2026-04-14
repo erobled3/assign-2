fb43d0c2dcfa3e906d1aacbab9bdbac1d318049e
A Student <student@nowhere.edu>
2026-04-14 03:56:32 +0000

    Apr 13, 2026, 8:56 PM

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
   
   const double PI = 3.14159265359;
-  const double GAVITY = 32.17405;
+  const double GRAVITY = 32.17405;
 
   //INPUT//
   int ramp_height = 12;
