1bd09c0a95ef7e3fa55363d8004747aae449eb6b
A Student <student@nowhere.edu>
2026-04-14 02:39:20 +0000

    Apr 13, 2026, 7:39 PM

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 //The height of the ramp above the landing area (in feet)//
 
+//The launch velocity of the bike (in feet per second)//
 
+The launch angle of the ramp (in degrees)//
 
 // parameters like: "doubleIT(int x)"" DO NOT use cin or cout!! they use "return!"//
