67230f4fd723a8168ee7d3c267bcbbacf893b05b
A Student <student@nowhere.edu>
2026-04-14 05:44:48 +0000

    Apr 13, 2026, 10:44 PM

--- main.cpp
+++ main.cpp
@@ -34,14 +34,14 @@ int main() {
   double two_times_gravity_height = (2 * GRAVITY * ramp_height);
   double air_ish_time = vertical_velocity + (sqrt(squared_vert_velocity + two_times_gravity_height));
   double time = air_ish_time/GRAVITY;
-  cout << "Time in the Air: " <<time<< "seconds." << endl;
+  cout << "Time in the Air: " <<time<< " seconds." << endl;
 
   //The maximum height reached during the jump//
 
 
   //The horizontal distance traveled before landing//
   double horizontal_distance = horizontal_velocity *time;
-  cout << "Jump Distance: " << horizontal_distance << " feet " << endl;
+  cout << "Jump Distance: " << horizontal_distance << " feet." << endl;
   //The speed of the bike at landing//
 
   //const double ACCELERATION_DUE_TO_GRAVITY = 32.17405;//
