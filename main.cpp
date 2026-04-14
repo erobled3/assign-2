2661a7d299db10988d20bb870e9d1b2110b1b22b
A Student <student@nowhere.edu>
2026-04-14 05:41:00 +0000

    Apr 13, 2026, 10:41 PM

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main() {
 
 
   //The horizontal distance traveled before landing//
-  
+  double horizontal_distance = 
   //The speed of the bike at landing//
 
   //const double ACCELERATION_DUE_TO_GRAVITY = 32.17405;//
