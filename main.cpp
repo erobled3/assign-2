679c764e48e7ae3ce2b7783bf068bb3ac9009fd7
A Student <student@nowhere.edu>
2026-04-14 03:48:11 +0000

    Apr 13, 2026, 8:48 PM

--- main.cpp
+++ main.cpp
@@ -22,8 +22,9 @@ int main() {
   //The horizontal speed of the bike when it leaves the ramp//
   cout << "Horizontal Velocity: " << double (bike_velocity * cos(angle_of_bike * PI/180));
 
-  //The time the bike stays in the air//
+  
   double (bike_velocity * sin(angle_of_bike * PI/180));
+  //The time the bike stays in the air//
   //The maximum height reached during the jump//
   //The horizontal distance traveled before landing//
   //The speed of the bike at landing//
