bb10074828ceefa758bd94e36005c5f24b7bc086
A Student <student@nowhere.edu>
2026-04-14 03:09:59 +0000

    Apr 13, 2026, 8:09 PM

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
 
   //THREE The launch angle of the ramp (in degrees)//
   int angle_of_bike = 20;
-  cout << "Angle of the Bike (in degrees): " << angle_of_bike << endl;
+  cout << "Bike angle (in degrees): " << angle_of_bike << endl;
 
   //OUTPUT//
   //The horizontal speed of the bike when it leaves the ramp//
