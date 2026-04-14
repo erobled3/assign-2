0bc3617496a8865127488a67b8ca7aed83b9e05d
A Student <student@nowhere.edu>
2026-04-14 05:49:26 +0000

    Apr 13, 2026, 10:49 PM

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main() {
   cout << "Time in the Air: " <<time<< " seconds." << endl;
 
   //The maximum height reached during the jump//
-  double squared_vert_velocity/()
+  double height = squared_vert_velocity/(2*GRAVITY)
 
   //The horizontal distance traveled before landing//
   double horizontal_distance = horizontal_velocity *time;
